317f116399ccdfcd7fb7ae069dfaf465f0f4c7d4
devjunayed <devjunayed@gmail.com>
2025-09-08 17:27:02 +0600

    implementing middleware with fake user data

---
+++ src/middleware.ts
@@ -0,0 +1,48 @@
+import { NextResponse, NextRequest } from "next/server";
+
+const AuthRoutes = ["/login", "/register"];
+
+type Role = keyof typeof roleBasedRoutes;
+
+  const roleBasedRoutes = {
+    USER: [/^\/profile/],
+    ADMIN: [/^\/admin/]
+  }
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const {pathname} = request.nextUrl;
+
+
+
+  const user = {
+      name: "Junayed",
+      token: "jjslfsd",
+      role: "ADMIN"
+  }
+
+//   const user = undefined;
+
+  if (!user) {
+    if (AuthRoutes.includes(pathname)) {
+      return NextResponse.next();
+    }else{
+        return NextResponse.redirect(new URL("/login", request.url))
+    }
+  }
+
+
+  if(user.role && roleBasedRoutes[user?.role as Role] ){
+    const routes = roleBasedRoutes[user.role as Role];
+
+    if(routes.some((route) => pathname.match(route))){
+        return NextResponse.next();
+    }
+  }
+
+  return NextResponse.redirect(new URL("/", request.url));
+}
+
+export const config = {
+  matcher: ["/profile/:path*", "/admin", "/login", "/register"],
+};
